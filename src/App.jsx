c86261a74f9aa001d9e2a767a6e3dadc3c603ac5
kiograco <kiograco@gmail.com>
2026-06-24 12:09:27 -0300

    fix: logo nova

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,7 @@ function App() {
         <nav className="topbar">
         <div className="brand-block">
   <img
-    src="/logo.png"
+    src="/logoNova.png"
     alt="Logo Andreia Lucynara"
     className="brand-logo"
   />
